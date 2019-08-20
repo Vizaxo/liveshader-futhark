a78dd25be91c38f84f1cc9a85a227ebbec4c57ad
Vizaxo <vizaxo@mailfence.com>
2019-08-20 01:33:16 +0100

    Call Futhark code from Python

--- liveshader.py
+++ liveshader.py
@@ -1,5 +1,15 @@
 #!/usr/bin/env python3
 
 import sys
+import numpy
+from subprocess import call
 
-print(str(sys.argv));
+filename = 'shader.fut' #sys.argv[1];
+# TODO: set up opencl
+call(['futhark', 'python', '--library', filename]);
+
+import shader
+
+shader = shader.shader()
+n = shader.average(numpy.array([6.0, 4.0, 5.0]))
+print(n)
---
+++ shader.fut
@@ -0,0 +1,1 @@
+entry average (xs: []f64) = reduce (+) 0.0 xs / r64 (length xs)
